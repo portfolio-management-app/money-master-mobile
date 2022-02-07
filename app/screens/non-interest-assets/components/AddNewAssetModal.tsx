6f2fee850a6bd6d639a5387cc58c4d9d21717307
hoaxvo <dev.h1620@gmail.com>
2022-02-07 14:30:39 +0700

    fix bug crash app when build

--- app/screens/non-interest-assets/components/AddNewAssetModal.tsx
+++ app/screens/non-interest-assets/components/AddNewAssetModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { BottomSheet } from 'react-native-elements';
 import { Button, View } from 'react-native-ui-lib';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { colorScheme } from 'styles';
 
 interface IProps {
@@ -10,12 +11,28 @@ interface IProps {
 }
 
 export const AddNewAssetModal = ({ show, onHide }: IProps) => {
+  const [date, setDate] = React.useState(new Date(1598051730000));
+
+  const onChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate || date;
+
+    setDate(currentDate);
+  };
+
   return (
     <BottomSheet
       containerStyle={{ backgroundColor: colorScheme.loading }}
       isVisible={show}
     >
       <View style={styles.modal}>
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
         <Button style={styles.button} onPress={onHide} label="Cancel" />
       </View>
     </BottomSheet>
