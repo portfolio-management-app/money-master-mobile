9e8345cdc8a500a23d82d2ac0f094d1af9c3fa8f
hoaxvo <dev.h1620@gmail.com>
2022-02-07 14:07:40 +0700

    add button shadow

--- app/components/floating-button/index.tsx
+++ app/components/floating-button/index.tsx
@@ -64,6 +64,15 @@ const getStyle = ({
     position: 'absolute',
     bottom: 20,
     right: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   };
   switch (placement) {
     case 'bottom-left':
