2e844d5dfc7cb7520ee54f8336209f75a3dd4808
hoaxvo <dev.h1620@gmail.com>
2022-03-04 17:51:07 +0700

    fix lost icon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { NavigationStack } from 'navigation/stack';
 import { MenuProvider } from 'react-native-popup-menu';
 import React from 'react';
+import { Assets } from 'react-native-ui-lib';
+console.log('Loaded asset', Assets);
 
 const App = () => {
   return (
